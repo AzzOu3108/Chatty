b5ca0c81bbba30b1514c5958e1a5ff00b4ac3ff6
Askelou Azzeddine <126351103+AzzOu3108@users.noreply.github.com>
2026-03-18 11:15:33 +0100

    update cookie config for cross-domain authentication

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -12,10 +12,10 @@ export const generateToken = (userId: string, res: Response) =>{
     });
 
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        secure: process.env.NODE_ENV !==" development ",
-        sameSite: "strict"
-    });
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none" 
+});
     return token;
 }
