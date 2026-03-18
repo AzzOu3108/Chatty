842f8c728087b2ba1cc0fda3b19dd732389177de
Askelou Azzeddine <126351103+AzzOu3108@users.noreply.github.com>
2026-03-18 11:07:21 +0100

    replace hardcoded BASE_URL with environment variable

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -4,7 +4,7 @@ import type { AuthStore, SignupBody, LoginBody } from "../types/Auth";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const useAuthStore = create<AuthStore>((set, get)=>({
     authUser: null,
